fa2ca130287cd4d83afb89c7c661035e708df574
OH-GITAEK <dhrlxor1512@gmail.com>
2024-01-30 22:34:35 +0900

    [fix] 배포를 위한 프로젝트urls 수정

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("apps.main.urls")),
     path('chatting-room',include("apps.chattings.urls")),
+
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
 ]
