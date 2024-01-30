684e2782cf27e81c2e1a8aca999a407c29e17160
Ubuntu <ubuntu@ip-172-31-35-15.ap-northeast-2.compute.internal>
2024-01-30 15:43:04 +0900

    feat: 리눅스 환경 설정

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -1,4 +1,24 @@
 """
+   WSGI config for config project.
+
+   It exposes the WSGI callable as a module-level variable named ``application``.
+
+   For more information on this file, see
+   https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+   # 추가
+import site
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+   # 추가
+site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+application = get_wsgi_application()
+
+"""
 WSGI config for server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -6,11 +26,3 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
-application = get_wsgi_application()
