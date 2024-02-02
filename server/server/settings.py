5d5f4ea98fa846253af2232d310925e1cd3ce838
OH-GITAEK <dhrlxor1512@gmail.com>
2024-02-02 22:46:52 +0900

    [fix] csrf관련 settings변경

--- server/server/settings.py
+++ server/server/settings.py
@@ -142,5 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+CSRF_TRUSTED_ORIGINS = ["http://hello.piro-ym.p-e.kr", "https://hello.piro-ym.p-e.kr"]
 
