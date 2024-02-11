4dcff49ba0e68eb25461903195d77ada3368fec2
woojin <zaza0804@uos.ac.kr>
2024-02-11 23:33:02 +0900

    [feat] 방생성 제한 및 에러 체크 기능

--- server/apps/chattings/models.py
+++ server/apps/chattings/models.py
@@ -5,7 +5,7 @@ from apps.main.models import User
 
 # Create your models here.
 class GameRoom(models.Model):
-    room_name = models.CharField(max_length=32)
+    room_name = models.CharField(max_length=32, unique=True)
 
     order_game = models.CharField(max_length=50)
     ran_figure = models.CharField(max_length=50, null = True, blank = True)#,로 구분된 문자열
@@ -14,3 +14,5 @@ class GameRoom(models.Model):
     ran_music = models.CharField(max_length=50, null = True, blank = True)
 
     participants = models.IntegerField(default = 0)
+    
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id")
