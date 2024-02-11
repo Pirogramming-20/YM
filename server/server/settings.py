e2f6d2f44cb06bda3a493939940c2ed49baa98e1
yunseo <chldbstj0219@naver.com>
2024-02-12 00:19:37 +0900

    fix:음악 버튼

--- server/server/settings.py
+++ server/server/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [
 # else:
 #     STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
-# STATIC_ROOT=os.path.join(BASE_DIR, 'static') # daphne 사용시 스테틱 루트를 스테틱으로 설정해야 css 파일 안깨지고 사용 가능
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') # runserver시 사용
+STATIC_ROOT=os.path.join(BASE_DIR, 'static') # daphne 사용시 스테틱 루트를 스테틱으로 설정해야 css 파일 안깨지고 사용 가능
+# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') # runserver시 사용
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
