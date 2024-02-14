b42cdf716d1f95eb0ed8daa299aec621598028b8
yunseo <chldbstj0219@naver.com>
2024-02-15 03:33:07 +0900

    fix:label 수정

--- server/server/settings.py
+++ server/server/settings.py
@@ -141,14 +141,8 @@ STATICFILES_DIRS = [
 # else:
 #     STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
-<<<<<<< HEAD
-STATIC_ROOT=os.path.join(BASE_DIR, 'static') # daphne 사용시 스테틱 루트를 스테틱으로 설정해야 css 파일 안깨지고 사용 가능
-#STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') # runserver시 사용
-=======
 # STATIC_ROOT=os.path.join(BASE_DIR, 'static') # daphne 사용시 스테틱 루트를 스테틱으로 설정해야 css 파일 안깨지고 사용 가능
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') # runserver시 사용 #배포시 사용
-
->>>>>>> c155621c47a38e19b29cd5c8f3dd0d51858465f5
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') # runserver시 사용
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
