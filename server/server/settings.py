cd2d2cd4ad90400e204841b50dd574e99ecc4f58
OH-GITAEK <dhrlxor1512@gmail.com>
2024-02-20 15:14:01 +0900

    fix : 채팅창 css수정

--- server/server/settings.py
+++ server/server/settings.py
@@ -85,24 +85,24 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '54.180.115.196',
-        'PORT': '5432',
-        'NAME': 'database-11',
-        'USER': 'postgres',
-        'PASSWORD': 'dhrlxor12*',
-    }
-}
- 
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': '54.180.115.196',
+#         'PORT': '5432',
+#         'NAME': 'database-11',
+#         'USER': 'postgres',
+#         'PASSWORD': 'dhrlxor12*',
 #     }
 # }
+ 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -153,8 +153,8 @@ STATICFILES_DIRS = [
 # else:
 #     STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
-# STATIC_ROOT=os.path.join(BASE_DIR, 'static') # daphne 사용시 스테틱 루트를 스테틱으로 설정해야 css 파일 안깨지고 사용 가능
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') # runserver시 사용 #배포시 사용
+STATIC_ROOT=os.path.join(BASE_DIR, 'static') # daphne 사용시 스테틱 루트를 스테틱으로 설정해야 css 파일 안깨지고 사용 가능
+# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles') # runserver시 사용 #배포시 사용
 
 
 # Default primary key field type
