de38407c02a3975c1c36147d9765f32054f1c428
OH-GITAEK <dhrlxor1512@gmail.com>
2024-03-10 12:23:15 +0900

    [fixzz]

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -43,15 +43,16 @@ async def join(sid, room_name,check):
         rooms[room_name] += 1  
     else:
         rooms[room_name] = 1
-    
-    await sio.emit("count", rooms[room_name], room=room_name)  
+    if check == 1:
+        await sio.emit('message', ["1번문제",""], room=room_name)
+    await sio.emit("count", [rooms[room_name],sid], room=room_name)  
     
 @sio.event
 async def join_again(sid, room_name):
     await sio.enter_room(sid, room_name)
     if not(room_name in rooms):
         rooms[room_name] = 1  
-    await sio.emit("count", rooms[room_name], room=room_name)  
+    await sio.emit("count", [rooms[room_name],sid], room=room_name)  
 
 @sio.event
 async def leave(sid, room_name):
@@ -81,7 +82,7 @@ async def disconnect(sid):
     for room_name in rooms:
         if sio.rooms(sid).__contains__(room_name):
             rooms[room_name] -= 1
-            await sio.emit('count', rooms[room_name], room=room_name)
+            await sio.emit('count', [rooms[room_name],sid], room=room_name)
     #몇초이상 접속 안할 경우 종료시킨다?
     # if rooms[room_name] == 0:
     #     print('탈출시도')
